2cde28796e9675a111be9f180d555c12079696b2
Marina <144258790+mor1la@users.noreply.github.com>
2025-02-02 23:08:11 +0300

    Update AVLNode.py

--- task1/AVLNode.py
+++ task1/AVLNode.py
@@ -1,5 +1,13 @@
 class Node:
     def __init__(self, val):
+        """
+        Инициализация узла в AVL-дереве.
+
+        :param val: Значение, которое будет храниться в узле.
+        :param height: Начальная высота узла, равная 1 (для листа дерева).
+        :param left: Ссылка на левое поддерево (изначально None).
+        :param right: Ссылка на правое поддерево (изначально None).
+        """
         self.val = val
         self.height = 1
         self.left = None
